4deaba0eb051abc7f99be50de0fc708436f91f02
L.Plshkv <liza.pleshakova@gmail.com>
2023-01-13 22:51:03 +0300

    V.2.0

--- scripts/index.js
+++ scripts/index.js
@@ -4,14 +4,15 @@ let popupElement = document.querySelector('.popup');
 
 let popupOpenButtonElement = document.querySelector('.profile__edit-btn');
 let popupCloseButtonElement = popupElement.querySelector('.popup__close-btn');
-let nameInput = popupElement.querySelector('.popup__input_form_usermane');
-let jobInput = popupElement.querySelector('.popup__input_form_description');
+let formInput = popupElement.querySelector('.popup__input-container');
+let nameInput = formInput.querySelector('.popup__input_form_usermane');
+let jobInput = formInput.querySelector('.popup__input_form_description');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__description');
 
 //Открытие попапа
 let popupOpen = function() {
-  popupElement.classList.add('popup__opened')
+  popupElement.classList.add('popup_opened')
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 };
@@ -20,11 +21,9 @@ popupOpenButtonElement.addEventListener('click', popupOpen);
 
 //Закрытие попапа
 let popupClose = function() {
-  popupElement.classList.remove('popup__opened')
+  popupElement.classList.remove('popup_opened')
 };
 popupCloseButtonElement.addEventListener('click', popupClose);
-
-
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -32,10 +31,11 @@ function handleFormSubmit (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
+    popupClose();
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-popupElement.addEventListener('submit', handleFormSubmit);
-popupElement.addEventListener('submit', popupClose);
+formInput.addEventListener('submit', handleFormSubmit);
+
 
